835ec5a1100051e0039c40b6b3b16055653a74ec
Nitesh Sahani <niteshsahani046@gmail.com>
2026-07-02 18:46:38 +0530

    Written a code for a simple calculator.

---
+++ src/D1_Basics/MiniCalculator.java
@@ -0,0 +1,30 @@
+package D1_Basics;
+//4. Take in two numbers and an operator (+, -, *, /) and calculate the value. (Use if conditions)
+import java.util.Scanner;
+public class MiniCalculator {
+    public static void main(String args[]) {
+        System.out.println("Enter the first number");
+        Scanner sc = new Scanner(System.in);
+        double A = sc.nextDouble();
+        System.out.println(" Choose the operator among * , /, - and +");
+        char operator = sc.next().charAt(0);
+        System.out.println("Enter the second number.");
+        double B = sc.nextDouble();
+        if (operator == '-') {
+            double Result = A - B;
+            System.out.println(Result);
+        } else if (operator == '+') {
+            double Result = A + B;
+            System.out.println(Result);
+        } else  if (operator == '/') {
+            double Result = A / B;
+            System.out.println(Result);
+        }  else  if (operator == '*') {
+            double Result = A * B;
+            System.out.println(Result);
+        } else {
+            System.out.println("Invalid input");
+        }
+
+    }
+}
