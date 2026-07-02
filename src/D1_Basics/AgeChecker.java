d137e8dcf735cda729ae67d1dec78568f221556b
Nitesh Sahani <niteshsahani046@gmail.com>
2026-07-02 13:08:34 +0530

    Age Checker for voting eligibility

---
+++ src/D1_Basics/AgeChecker.java
@@ -0,0 +1,14 @@
+package D1_Basics;
+import java.util.Scanner;
+public class AgeChecker {
+    public static void main(String args[]){
+         Scanner sc = new Scanner(System.in);
+         System.out.println("Enter your age");
+         int Age = sc.nextInt();
+         if (Age >= 18) {
+             System.out.println("You are eligible for voting. ");
+         } else {
+             System.out.println("You are not eligible for voting yet.");
+         }
+    }
+}
