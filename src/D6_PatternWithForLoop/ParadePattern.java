9e0994332853b43ca7a2898791d792823ee20d95
Nitesh Sahani <niteshsahani046@gmail.com>
2026-07-07 16:26:33 +0530

    Did some problems related to for loop.

---
+++ src/D6_PatternWithForLoop/ParadePattern.java
@@ -0,0 +1,20 @@
+package D6_PatternWithForLoop;
+//print
+//*****
+//*****
+//*****
+//*****
+//*****
+public class ParadePattern {
+    public static void main(String args[]){
+        int count = 0;
+        for (int i = 1; i <= 25; i++){
+            System.out.print("*");
+            count++;
+            if (count%5 == 0) {
+                System.out.println();
+            }
+                   }
+
+    }
+}
---
+++ src/D6_PatternWithForLoop/ParadePattern2.java
@@ -0,0 +1,15 @@
+package D6_PatternWithForLoop;
+//print
+//*****
+//*****
+//*****
+//*****
+//*****
+public class ParadePattern2 {
+    public static void main(String args[]){
+            for (int i = 1; i <= 5; i++){
+            System.out.println("*****");
+            }}
+                   }
+
+
